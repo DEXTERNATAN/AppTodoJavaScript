295c7267d8d88d943b90190d75f1ddc6d91c5248
Natanael de Sousa Leite <natanael.leite@serpro.gov.br>
2019-09-30 09:49:18 -0300

    Adiciona funcionalidades de javascript para teste

--- curso-es6/main.js
+++ curso-es6/main.js
@@ -1,9 +1,69 @@
-alert("Teste");
+// const = constante o valor não pode ser reatribuido
+// let = o valor pode ser reatribuido
 
-class Teste {
-  metodo() {}
+// A mutação pode acontecer numa constante, voce muda o valor da constante atraves do objeto.
+// const usuario = { nome: "Natanael" };
+// usuario.nome = "Diego";
+// console.log(usuario);
 
-  outroMetodos() {}
+// Classes
+class List {
+  constructor() {
+    this.data = [];
+  }
 
-  outroMetodos2() {}
+  add(data) {
+    this.data.push(data);
+    console.log("Resultado: ", this.data);
+  }
 }
+
+class TodoList extends List {
+  constructor() {
+    super();
+    this.usuario = "Diego";
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+}
+
+var MinhaList = new TodoList();
+
+document.getElementById("novotodo").onclick = function() {
+  MinhaList.add("Novo todo");
+};
+
+MinhaList.mostraUsuario();
+
+// Funções do ES6
+const arr = [1, 3, 4, 5, 8, 9];
+
+// Função map -
+const newArr = arr.map(function(item, index) {
+  return item + index;
+});
+
+console.log(newArr);
+
+// Função reduce -
+const sum = arr.reduce(function(total, next) {
+  return total + next;
+});
+
+console.log(sum);
+
+// Função filter - usada para filtrar o array
+const filter = arr.filter(function() {
+  return item % 2 === 0;
+});
+
+console.log(filter);
+
+// Find - verificar se existe a informação dentro do array
+const find = arr.find(function() {
+  return item === 4;
+});
+
+console.log(find);
